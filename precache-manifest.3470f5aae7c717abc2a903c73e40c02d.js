4d7ec8f8229514856252631abf68440203dc8f55
gitName <a@b.c>
2020-07-14 16:29:17 +0530

    Updates

---
+++ precache-manifest.3470f5aae7c717abc2a903c73e40c02d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a44c8eac0eb6ff9d9cda57d36e13a3d",
+    "url": "/Mishcon/index.html"
+  },
+  {
+    "revision": "03c4775f92439c076950",
+    "url": "/Mishcon/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "899190806c3e3bc23b1d",
+    "url": "/Mishcon/static/js/2.926a08f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Mishcon/static/js/2.926a08f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03c4775f92439c076950",
+    "url": "/Mishcon/static/js/main.d7132f04.chunk.js"
+  },
+  {
+    "revision": "c1084b77d5cbe77cf5d3",
+    "url": "/Mishcon/static/js/runtime-main.5fd69361.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Mishcon/static/media/logo.5d5d9eef.svg"
+  }
+]);
